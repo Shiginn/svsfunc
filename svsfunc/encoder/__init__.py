5fc7baae22b297189942bb34c61100c3d9b10985
Shigin <shigin.contact@gmail.com>
2022-07-27 23:22:48 +0200

    expose encoder.utils module

--- svsfunc/encoder/__init__.py
+++ svsfunc/encoder/__init__.py
@@ -2,4 +2,5 @@ from .base import *  # noqa: F401, F403
 from .video import *  # noqa: F401, F403
 from .audio import *  # noqa: F401, F403
 from .chapters import *  # noqa: F401, F403
+from .utils import *  # noqa: F401, F403
 from .encode import *  # noqa: F401, F403
