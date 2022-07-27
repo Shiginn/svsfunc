953192d4a18bd7b30744ed8a2fa7374133571549
Shigin <shigin.contact@gmail.com>
2022-07-27 03:30:57 +0200

    add vs import in conf

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,9 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+autosummary_generate = True
+
+autodoc_mock_imports = ["vapoursynth"]
 
 # -- Options for todo extension ----------------------------------------------
 
