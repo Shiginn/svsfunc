4fe589de317d3e11b3853765d7d0557f21b3640f
Shigin <shigin.contact@gmail.com>
2022-10-15 18:25:20 +0200

    encoder: update base encoder - move get_offset to base - add get_func_name

--- svsfunc/tooling/base.py
+++ svsfunc/tooling/base.py
@@ -1,5 +1,7 @@
 __all__ = ["BaseEncoder"]
 
+import inspect
+
 import vapoursynth as vs
 from vardautomation import FileInfo
 
@@ -18,3 +20,24 @@ class BaseEncoder:
     def __init__(self, file: FileInfo, clip: vs.VideoNode) -> None:
         self.file = file
         self.clip = clip
+
+
+    def get_offset(self) -> int:
+        """
+        Get offset based on FileInfo trims
+        """
+        trims = self.file.trims_or_dfs
+
+        if trims is None:
+            offset = 0
+        elif isinstance(trims, tuple):
+            offset = trims[0]
+        elif isinstance(trims, list) and isinstance(trims[0], tuple):
+            offset = trims[0][0]
+
+        return 0 if offset is None else offset * -1
+
+
+    @staticmethod
+    def get_func_name() -> str:
+        return inspect.stack()[1][3]
--- svsfunc/tooling/chapters.py
+++ svsfunc/tooling/chapters.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 __all__ = ["ChapterTooling"]
 
-from typing import List, NoReturn, Sequence, Type, Union, cast
+from typing import List, Sequence, Type, Union, cast
 
 from vardautomation import Chapter, MatroskaXMLChapters, OGMChapters, VPath
 
@@ -53,26 +53,4 @@ class ChapterTooling(BaseEncoder):
         if chapters_names is not None:
             chapter_file.set_names(chapters_names)
 
-        chapter_file.shift_times(self._get_offset(shift_time), self.clip.fps)
-
-
-    def _get_offset(self, offset: int | None = None) -> int | NoReturn:
-        if offset is not None:
-            return offset
-
-        trim = self.file.trims_or_dfs
-
-        if isinstance(trim, tuple):
-            offset = trim[0]
-        elif isinstance(trim, list) and isinstance(trim[0], tuple):
-            offset = trim[0][0]
-        else:
-            raise ValueError("ChapterEncoder._get_offset: cound not detect offset, set it manually.")
-
-
-        if trim is None or offset is None:
-            offset = 0
-        else:
-            offset = offset * -1
-
-        return offset
+        chapter_file.shift_times(shift_time or self.get_offset(), self.clip.fps)
