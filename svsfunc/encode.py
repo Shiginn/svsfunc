fd2620b2f96a0a86b4c52d2394e09756d96361d0
Shigin <shigin.contact@gmail.com>
2022-10-15 18:51:43 +0200

    encoder: update encode.py
    - add type hints to class properties
    - add option to change audio track order
    - fix muxer options type
    - add temp file to work files when using FileInfo2
    - update audio track selector, now uses tracks and tools

--- svsfunc/encode.py
+++ svsfunc/encode.py
@@ -1,9 +1,9 @@
 __all__ = ["Encoder"]
 
-from typing import Any, List, NoReturn, Sequence, Tuple, TypeVar
+from typing import List, NoReturn, Sequence, Tuple, TypeVar
 
 from vardautomation import (
-    UNDEFINED, AnyPath, AudioTrack, ChaptersTrack, FileInfo, Lang, MatroskaFile, RunnerConfig, SelfRunner, Track,
+    UNDEFINED, AnyPath, AudioTrack, ChaptersTrack, FileInfo2, Lang, MatroskaFile, RunnerConfig, SelfRunner, Track,
     VideoTrack, VPath, logger
 )
 
@@ -12,12 +12,13 @@ from .tooling.chapters import ChapterTooling
 from .tooling.utils import UtilsTooling
 from .tooling.video import VideoTooling
 
-
 T = TypeVar("T")
 
 
 class Encoder(VideoTooling, AudioTooling, ChapterTooling, UtilsTooling):
     """Generate an encoding chain"""
+    mux: MatroskaFile | None = None
+    runner: SelfRunner
 
     def muxer(
         self,
@@ -25,17 +26,20 @@ class Encoder(VideoTooling, AudioTooling, ChapterTooling, UtilsTooling):
         a_title: str | List[str | None] | None = None,
         a_lang: Lang | List[Lang] = UNDEFINED,
         external_audio: List[AnyPath | Tuple[AnyPath, str | None, Lang]] | None = None,
-        **muxer_options: Any
+        audio_tracks_order: List[int] | None = None,
+        *muxer_overrides: str
     ) -> None:
         """
         Set parameters for the muxer.
 
-        :param v_title:         Video track name.
-        :param a_title:         Audio track(s) name(s).
-        :param a_lang:          Audio track(s) language(s).
-        :param external_audio:  Add external audio tracks. Can be a list of path, or a list of tuple
-                                (path, track_name, track_lang).
-        :param muxer_options:   Additional paramters to be passed to the muxer.
+        :param v_title:             Video track name.
+        :param a_title:             Audio track(s) name(s).
+        :param a_lang:              Audio track(s) language(s).
+        :param external_audio:      Add external audio tracks. Can be a list of path, or a list of tuple
+                                    (path, track_name, track_lang).
+        :param audio_track_order:   Change audio track order on ouput file. Uses same indexes as AudioEncoder.set_tracks
+                                    If external audios are added, the track number is defined as max(current_tracks) + 1
+        :param muxer_options:       Additional paramters to be passed to the muxer.
         """
 
         if not isinstance(a_title, List):
@@ -51,30 +55,42 @@ class Encoder(VideoTooling, AudioTooling, ChapterTooling, UtilsTooling):
         logger.info(f"Muxing video file: {self.file.name_clip_output} (track name: {v_title})")
         tracks: List[Track] = [VideoTrack(self.file.name_clip_output, v_title)]
 
-        a_srcs = list(zip(self.output_tracks, a_title, a_lang))
+        a_tracks = list[AudioTrack]()
 
-        a_track = self._select_a_track(self.file)
-        for idx, name, lang in a_srcs:
-            logger.info(f"Muxing audio track {idx} (track name: {name}, track lang: {lang.name}-{lang.iso639})")
-            tracks.append(AudioTrack(a_track.set_track(idx), name, lang))
+        if self.output_tracks:
+            a_srcs = list(zip(self.output_tracks, a_title, a_lang))
+            file_track = self._select_a_track()
+            for idx, name, lang in a_srcs:
+                logger.info(f"Muxing audio track {idx} (track name: {name}, track lang: {lang.name}-{lang.iso639})")
+                a_tracks.append(AudioTrack(file_track.set_track(idx), name, lang))
 
         if external_audio is not None:
             for track in external_audio:
-                if isinstance(track, tuple):
-                    path, name, lang = track
-                else:
-                    path = track
-                    name = None
-                    lang = UNDEFINED
+                self.track_number += 1
+                self.input_tracks.append(max(self.input_tracks) + 1)  # [2, 4] ->  [2, 4, 5]
+                self.output_tracks.append(max(self.output_tracks) + 1)
 
+                if not isinstance(track, tuple):
+                    track = (track, None, UNDEFINED)
+
+                path, name, lang = track
                 logger.info(f"Muxing audio file {path} (track name: {name}, track lang: {lang.name}-{lang.iso639})")
-                tracks.append(AudioTrack(path, name, lang))
+                a_tracks.append(AudioTrack(path, name, lang))
+
+        if audio_tracks_order is not None:
+            if len(audio_tracks_order) != self.track_number:
+                raise ValueError("Encoder.muxer: missing audio tracks in audio_tracks_order.")
+
+            logger.info(f"Output track order : {audio_tracks_order}")
+            self.output_tracks = audio_tracks_order
+
+        tracks += [a_tracks[i - 1] for i in self.output_tracks]
 
         if self.file.chapter:
             logger.info(f"Muxing chapter file: {self.file.chapter}")
             tracks.append(ChaptersTrack(self.file.chapter))
 
-        self.mux = MatroskaFile(self.file.name_file_final, tracks, **muxer_options)
+        self.mux = MatroskaFile(self.file.name_file_final, tracks, *muxer_overrides)
 
 
     def run(self, order: RunnerConfig.Order = RunnerConfig.Order.VIDEO) -> None:
@@ -97,6 +113,10 @@ class Encoder(VideoTooling, AudioTooling, ChapterTooling, UtilsTooling):
         )
 
         self.runner = SelfRunner(self.clip, self.file, config)
+
+        if self.a_encoder and isinstance(self.file, FileInfo2):
+            for file in (self.file.a_src_cut.set_track(i) for i in self.input_tracks if self.file.a_src_cut):
+                self.runner.work_files.add(file)
 
         if self.post_filterchain_func is not None:
             self.runner.plp_function = self.post_filterchain_func
@@ -146,13 +166,16 @@ class Encoder(VideoTooling, AudioTooling, ChapterTooling, UtilsTooling):
         self.runner.work_files.clear()
 
 
-    @staticmethod
-    def _select_a_track(file: FileInfo) -> VPath | NoReturn:
-        for t in (file.a_enc_cut, file.a_src_cut, file.a_src):
-            if t is not None:
-                return t
+    def _select_a_track(self) -> VPath | NoReturn:
+        """Returns audio track that matches audio tooling."""
+        tracks = (self.file.a_enc_cut, self.file.a_src_cut, self.file.a_src)
+        a_tools = (self.a_encoder, self.a_cutter, self.a_extracter)
 
-        raise ValueError("Encoder._select_a_track: could not detect audio from source file")
+        for track, tool in zip(tracks, a_tools):
+            if track is not None and tool is not None:
+                return track
+
+        raise ValueError("Encoder._select_a_track: could not select audio from source file")
 
 
     @staticmethod
