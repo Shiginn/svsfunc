2691a8882a7c804bb21ce9eeab253ebe2b1aa182
Shigin <shigin.contact@gmail.com>
2022-10-29 21:38:45 +0200

    make write_props its own function

--- svsfunc/tooling/utils.py
+++ svsfunc/tooling/utils.py
@@ -35,27 +35,13 @@ class UtilsTooling(BaseEncoder):
             logger.info("Removed old comps folder")
 
 
-        def _write_props(clip: vs.VideoNode) -> vs.VideoNode:
-            def _get_props(n: int, f: vs.VideoFrame, clip: vs.VideoNode) -> vs.VideoNode:
-                txt = f"Frame Info:\nFrame Number: {n}"
-
-                pict_type = cast(bytes | None, f.props.get("_PictType"))
-                if (pict_type):
-                    txt += f"\nPicture Type: {pict_type.decode()}"
-
-                return clip.text.Text(txt, 7, 1)
-
-            f = partial(_get_props, clip=clip)
-            return clip.std.FrameEval(f, prop_src=clip)
-
-
         lossless = self.file.name_clip_output.append_stem("_lossless.mkv")
         filtered = core.lsmas.LWLibavSource(lossless.to_str()) if lossless.exists() else self.clip
         make_comps(
             {
-                "source": _write_props(self.file.clip_cut),
-                "filtered": _write_props(filtered),
-                "encode": _write_props(core.lsmas.LWLibavSource(self.file.name_file_final.to_str())),
+                "source": self.write_props(self.file.clip_cut),
+                "filtered": self.write_props(filtered),
+                "encode": self.write_props(core.lsmas.LWLibavSource(self.file.name_file_final.to_str())),
             },
             **args
         )
@@ -85,3 +71,18 @@ class UtilsTooling(BaseEncoder):
 
         if delete_index:
             os.remove(f"{self.file.name_file_final.to_str()}.lwi")
+
+
+    @staticmethod
+    def write_props(clip: vs.VideoNode) -> vs.VideoNode:
+        def _get_props(n: int, f: vs.VideoFrame, clip: vs.VideoNode) -> vs.VideoNode:
+            txt = f"Frame Info:\nFrame Number: {n}"
+
+            pict_type = cast(bytes | None, f.props.get("_PictType"))
+            if (pict_type):
+                txt += f"\nPicture Type: {pict_type.decode()}"
+
+            return clip.text.Text(txt, 7, 1)
+
+        f = partial(_get_props, clip=clip)
+        return clip.std.FrameEval(f, prop_src=clip)
