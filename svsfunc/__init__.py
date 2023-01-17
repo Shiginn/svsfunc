6e3102f6d2996f29bf39a87cb95731d9ddf2d67f
Shigin <shigin.contact@gmail.com>
2023-01-17 21:36:25 +0100

    add waifu2x submodule

--- svsfunc/__init__.py
+++ svsfunc/__init__.py
@@ -1,4 +1,5 @@
-from .denoise import *  # noqa: F401, F403
 from .encode import *  # noqa: F401, F403
 from .filtering import *  # noqa: F401, F403
+from .merge import *  # noqa: F401, F403
 from .parse import *  # noqa: F401, F403
+from .waifu2x import *  # noqa: F401, F403
