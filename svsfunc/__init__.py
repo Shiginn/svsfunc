df5a376ff694b02c1b358799c2aada3177b80915
Shigin <shigin.contact@gmail.com>
2023-04-24 19:51:45 +0200

    remove waifu2x.py

--- svsfunc/__init__.py
+++ svsfunc/__init__.py
@@ -2,5 +2,4 @@ from .encode import *  # noqa: F401, F403
 from .filtering import *  # noqa: F401, F403
 from .parse import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
-from .waifu2x import *  # noqa: F401, F403
 from .indexer import *  # noqa: F401, F403
