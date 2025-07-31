1d523d21aa21fdc6ab1a7a421776709659dd9fca
Shigin <shigin.contact@gmail.com>
2025-08-01 01:07:13 +0200

    yeet deprecated encoder/tooling module

--- svsfunc/__init__.py
+++ svsfunc/__init__.py
@@ -3,15 +3,3 @@ from .filterchain import *  # noqa: F401, F403
 from .episode_info import *  # noqa: F401, F403
 from .parse import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
-
-try:
-    # only import Encoder class if vardautomation is installed on the system
-    # will be completly removed later
-    import vardautomation  # noqa: F401
-    from .encode import *  # noqa: F401, F403
-
-    import warnings
-    warnings.warn("Vardautomation is deprecated, please use to vs-muxtools instead.", category=DeprecationWarning)
-
-except ImportError:
-    ...
--- svsfunc/tooling/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import *  # noqa: F401, F403
-from .video import *  # noqa: F401, F403
-from .audio import *  # noqa: F401, F403
-from .chapters import *  # noqa: F401, F403
-from .utils import *  # noqa: F401, F403
-
-__all__ = ["BaseEncoder", "VideoTooling", "AudioTooling", "ChapterTooling", "UtilsTooling"]  # noqa: F405
