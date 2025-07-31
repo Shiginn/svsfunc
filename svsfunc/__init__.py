fc9bc369c4dab23b3fe4d70c35cd8e79140d093b
Shigin <shigin.contact@gmail.com>
2025-07-31 22:46:59 +0200

    refactor EpisodeInfo - do not index given file anymore, accept videonode or src_file - updated clip retrival API to match src_file (src, src_cut, init, init_cut) - add option to get untrimmed ncop/nced

--- svsfunc/__init__.py
+++ svsfunc/__init__.py
@@ -1,6 +1,6 @@
 from .bdmv import *  # noqa: F401, F403
 from .filterchain import *  # noqa: F401, F403
-from .indexer import *  # noqa: F401, F403
+from .episode_info import *  # noqa: F401, F403
 from .parse import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
 
