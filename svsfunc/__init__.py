cd5275d8498962236be27ef1dc4a969a4a9b6421
Shigin <shigin.contact@gmail.com>
2025-08-01 02:04:47 +0200

    chore: replace flake8 with ruff

--- svsfunc/__init__.py
+++ svsfunc/__init__.py
@@ -1,5 +1,5 @@
 from .bdmv import *  # noqa: F401, F403
-from .filterchain import *  # noqa: F401, F403
 from .episode_info import *  # noqa: F401, F403
+from .filterchain import *  # noqa: F401, F403
 from .parse import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
